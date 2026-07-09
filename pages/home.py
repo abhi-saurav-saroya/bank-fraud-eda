1bd82fb101e860d661433ee971ee8213bd445cbd
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-10 00:07:16 +0530

    feat(pages/home): render overview and tech stack section

--- pages/home.py
+++ pages/home.py
@@ -2,12 +2,22 @@ import streamlit as st
 
 from components.hero import render as render_hero
 from components.metric_cards import render as render_metric_cards
+from components.overview import render as render_overview
+from components.tech_stack import render as render_tech_stack
 
 
 def render(df):
 
     render_hero(df)
 
-    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
 
     render_metric_cards(df)
+
+    st.markdown("<br><br>", unsafe_allow_html=True)
+
+    render_overview()
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    render_tech_stack()
