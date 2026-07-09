4432b0e5c757571847a94f173a28dbcb9cebddcc
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-09 21:34:58 +0530

    feat(pages/home): render metric cards in home page

--- pages/home.py
+++ pages/home.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
 from components.hero import render as render_hero
+from components.metric_cards import render as render_metric_cards
 
 
 def render(df):
 
     render_hero(df)
 
-    st.write("Home page under development...")
+    st.markdown('<br>', unsafe_allow_html=True)
+
+    render_metric_cards(df)
