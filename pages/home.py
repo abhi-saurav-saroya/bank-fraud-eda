7bb118a942c2f8f620f4ae6992cc9a8ebca46a62
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-11 00:09:38 +0530

    fix(pages/home): fixed module name while importing function

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from components.hero import render as render_hero
+from components.home_header import render as render_hero
 from components.home_metric_cards import render as render_home_metric_cards
 from components.overview import render as render_overview
 from components.tech_stack import render as render_tech_stack
