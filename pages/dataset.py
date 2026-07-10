3a5a465afa8224c2c280f1cba92c883e1abcbe7c
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-10 23:03:08 +0530

    feat(pages/dataset): render the column explorer section

--- pages/dataset.py
+++ pages/dataset.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from components.dataset_header import render as render_dataset_header
 from components.dataset_preview import render as render_dataset_preview
-# from components.column_info import render as render_column_info
+from components.column_explorer import render as render_column_explorer
 # from components.feature_summary import render as render_feature_summary
 
 
@@ -19,8 +19,8 @@ def render(raw_df, clean_df):
 
     st.markdown("<br>", unsafe_allow_html=True)
 
-    # render_column_info(clean_df)
+    render_column_explorer(clean_df)
 
-    # st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
 
     # render_feature_summary(clean_df)
