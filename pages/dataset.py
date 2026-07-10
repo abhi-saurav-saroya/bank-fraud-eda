3d8886b1ebce3060f958c9f912e7f5eabcf9b3d4
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-10 14:08:21 +0530

    feat(pages/dataset): render dataset header in new page

---
+++ pages/dataset.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+from components.dataset_header import render as render_dataset_header
+# from components.dataset_preview import render as render_dataset_preview
+# from components.column_info import render as render_column_info
+# from components.feature_summary import render as render_feature_summary
+
+
+def render(raw_df, clean_df):
+    """
+    Render Dataset Explorer page.
+    """
+
+    render_dataset_header(raw_df, clean_df)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    # render_dataset_preview(clean_df)
+
+    # st.markdown("<br>", unsafe_allow_html=True)
+
+    # render_column_info(clean_df)
+
+    # st.markdown("<br>", unsafe_allow_html=True)
+
+    # render_feature_summary(clean_df)
