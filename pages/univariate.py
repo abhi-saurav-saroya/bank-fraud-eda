32c6dcb29d9d4b86b2424b8d0a340667618cc78c
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-11 21:43:47 +0530

    feat(pages/univariate): render feature selector section

--- pages/univariate.py
+++ pages/univariate.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Components
 from components.univariate_header import render as render_header
-# from components.feature_selector import render as render_feature_selector
+from components.feature_selector import render as render_feature_selector
 # from components.summary_cards import render as render_summary_cards
 # from components.distribution_section import render as render_distribution_section
 # from components.statistics_table import render as render_statistics_table
@@ -17,12 +17,8 @@ def render(df):
     render_header()
 
     st.markdown("<br>", unsafe_allow_html=True)
-
-    # --------------------------------------------------
-    # Feature Selector
-    # --------------------------------------------------
-
-    # feature_type, feature = render_feature_selector(df)
+    
+    feature_type, feature = render_feature_selector(df)
 
     # --------------------------------------------------
     # Summary Cards
