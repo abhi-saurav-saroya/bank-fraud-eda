3e9ad3c7b661b97e3fb06eacc7a98e7ae6b053f2
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-11 14:56:42 +0530

    feat(plots/bar_chart): add a bar chart create method

---
+++ plots/bar_chart.py
@@ -0,0 +1,39 @@
+import plotly.express as px
+
+from plots.theme import COLORS, apply_theme
+
+
+def create(df, column, top_n=None):
+    """
+    Create a frequency bar chart.
+    """
+
+    counts = (
+        df[column]
+        .value_counts()
+        .reset_index()
+    )
+
+    counts.columns = [column, "Count"]
+
+    if top_n:
+        counts = counts.head(top_n)
+
+    fig = px.bar(
+        counts,
+        x=column,
+        y="Count",
+        color=column,
+        color_discrete_sequence=COLORS,
+    )
+
+    fig.update_traces(
+        hovertemplate="<b>%{x}</b><br>Count: %{y}<extra></extra>",
+    )
+
+    fig.update_layout(
+        title=f"{column} Frequency",
+        showlegend=False,
+    )
+
+    return apply_theme(fig)
