7a07057ec829dc8ad9ef4652ee80e4336f326416
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-11 14:56:06 +0530

    feat(plots/histogram): add a create histogram method

---
+++ plots/histogram.py
@@ -0,0 +1,28 @@
+import plotly.express as px
+
+from plots.theme import COLORS, PRIMARY, apply_theme
+
+
+def create(df, column, bins=30):
+    """
+    Create a histogram for a numerical feature.
+    """
+
+    fig = px.histogram(
+        df,
+        x=column,
+        nbins=bins,
+        color_discrete_sequence=[PRIMARY],
+    )
+
+    fig.update_traces(
+        hovertemplate="<b>%{x}</b><br>Count: %{y}<extra></extra>",
+        marker_line_width=0.5,
+    )
+
+    fig.update_layout(
+        title=f"Distribution of {column}",
+        bargap=0.05,
+    )
+
+    return apply_theme(fig)
