f9a6df14b300e7d14be5b2f19a32ebf626ce11e6
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-11 15:41:18 +0530

    feat(utils/feature-config): add a utility function to categorise the features into feature groups

---
+++ utils/feature_config.py
@@ -0,0 +1,104 @@
+import pandas as pd
+
+
+EXCLUDED_COLUMNS = {
+    "transaction_id",
+    "customer_id",
+    "transaction_datetime"
+}
+
+
+def get_binary_columns(df: pd.DataFrame) -> list[str]:
+    """
+    Return columns containing only binary values (0/1).
+    """
+
+    binary_columns = []
+
+    for column in df.columns:
+
+        if column in EXCLUDED_COLUMNS:
+            continue
+
+        values = set(df[column].dropna().unique())
+
+        if values <= {0, 1}:
+            binary_columns.append(column)
+
+    return sorted(binary_columns)
+
+
+def get_numeric_columns(df: pd.DataFrame) -> list[str]:
+    """
+    Return numerical columns excluding binary features.
+    """
+
+    numeric = list(
+        df.select_dtypes(include="number").columns
+    )
+
+    binary = get_binary_columns(df)
+
+    return sorted(
+        [
+            col
+            for col in numeric
+            if col not in binary
+            and col not in EXCLUDED_COLUMNS
+        ]
+    )
+
+
+def get_categorical_columns(df: pd.DataFrame) -> list[str]:
+    """
+    Return categorical (object/category) columns.
+    """
+
+    categorical = list(
+        df.select_dtypes(
+            include=["object", "category"]
+        ).columns
+    )
+
+    return sorted(
+        [
+            col
+            for col in categorical
+            if col not in EXCLUDED_COLUMNS
+        ]
+    )
+
+
+def get_datetime_columns(df: pd.DataFrame) -> list[str]:
+    """
+    Return datetime columns.
+    """
+
+    datetime_cols = list(
+        df.select_dtypes(
+            include=["datetime", "datetimetz"]
+        ).columns
+    )
+
+    return sorted(
+        [
+            col
+            for col in datetime_cols
+            if col not in EXCLUDED_COLUMNS
+        ]
+    )
+
+
+
+# Master Configuration
+def get_feature_groups(df: pd.DataFrame) -> dict:
+    """
+    Return all feature groups.
+    """
+
+    return {
+        "Numerical": get_numeric_columns(df),
+        "Categorical": get_categorical_columns(df),
+        "Binary": get_binary_columns(df),
+        "Datetime": get_datetime_columns(df),
+    }
