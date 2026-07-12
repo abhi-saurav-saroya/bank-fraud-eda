e4818b9663cb20432b8400e326a871c9f565e51d
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-13 00:19:01 +0530

    feat(plots/boxplot): modify boxplot to work for both univariate and bivariate analysis

--- plots/boxplot.py
+++ plots/boxplot.py
@@ -3,28 +3,60 @@ import plotly.express as px
 from plots.theme import PRIMARY, apply_theme
 
 
-def create(df, column):
+def create(df, column=None, x=None, y=None):
     """
     Create a box plot.
     """
 
-    fig = px.box(
-        df,
-        y=column,
-        color_discrete_sequence=[PRIMARY],
-        points="outliers",
-    )
 
-    fig.update_traces(
-        hovertemplate="<b>%{y}</b><extra></extra>",
-        hoverlabel=dict(
-            bgcolor="white",
-            font=dict(color="#000000")
+    # Univariate
+    if column is not None:
+        fig = px.box(
+            df,
+            y=column,
+            color_discrete_sequence=[PRIMARY],
+            points="outliers",
         )
-    )
 
-    fig.update_layout(
-        title=f"Box Plot of {column.replace('_', ' ').title()}",
-    )
+        fig.update_traces(
+            hovertemplate="<b>%{y}</b><extra></extra>",
+            hoverlabel=dict(
+                bgcolor="white",
+                font=dict(color="#000000"),
+            ),
+        )
+
+        fig.update_layout(
+            title=f"Box Plot of {column.replace('_', ' ').title()}",
+        )
+
+
+    # Bivariate
+    else:
+        fig = px.box(
+            df,
+            x=x,
+            y=y,
+            color=x,
+            points="outliers",
+        )
+
+        fig.update_traces(
+            hovertemplate=(
+                f"<b>{x.replace('_', ' ').title()}</b>: %{{x}}"
+                f"<br><b>{y.replace('_', ' ').title()}</b>: %{{y}}"
+                "<extra></extra>"
+            ),
+            hoverlabel=dict(
+                bgcolor="white",
+                font=dict(color="#000000"),
+            ),
+        )
+
+        fig.update_layout(
+            title=f"{y.replace('_', ' ').title()} by {x.replace('_', ' ').title()}",
+            showlegend=False,
+        )
+
 
     return apply_theme(fig)
