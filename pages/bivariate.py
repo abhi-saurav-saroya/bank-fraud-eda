493670ac81207a9b2f243f6c2a0a8c86703bc1d4
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-13 00:38:12 +0530

    feat(pages/bivariate): render the new section and receive correct return from older section

--- pages/bivariate.py
+++ pages/bivariate.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from components.bivariate.header import render as render_header
 from components.bivariate.variable_selector import render as render_variable_selector
+from components.bivariate.relationship_visualization import render as render_relationship_visualization
 
 def render(df):
     """
@@ -12,4 +13,8 @@ def render(df):
 
     st.markdown("<br>", unsafe_allow_html=True)
 
-    render_variable_selector(df)
+    x_feature, y_feature, x_type, y_type = render_variable_selector(df)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    render_relationship_visualization(df, x_feature, y_feature, x_type, y_type)
