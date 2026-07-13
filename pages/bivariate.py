f995d2d157268ddf09daaa758adb5788f1e9de7b
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-13 15:30:51 +0530

    style(pages/bivariate): add dividers instead of line breaks

--- pages/bivariate.py
+++ pages/bivariate.py
@@ -4,6 +4,7 @@ from components.bivariate.header import render as render_header
 from components.bivariate.variable_selector import render as render_variable_selector
 from components.bivariate.relationship_visualization import render as render_relationship_visualization
 from components.bivariate.relationship_summary import render as render_relationship_summary
+from components.bivariate.insight_panel import render as render_insight_panel
 
 def render(df):
     """
@@ -12,14 +13,18 @@ def render(df):
     
     render_header()
 
-    st.markdown("<br>", unsafe_allow_html=True)
+    st.divider()
 
     x_feature, y_feature, x_type, y_type = render_variable_selector(df)
 
-    st.markdown("<br>", unsafe_allow_html=True)
+    st.divider()
 
     render_relationship_visualization(df, x_feature, y_feature, x_type, y_type)
 
-    st.markdown("<br>", unsafe_allow_html=True)
+    st.divider()
 
     render_relationship_summary(df, x_feature, y_feature, x_type, y_type)
+
+    st.divider()
+
+    render_insight_panel(df, x_feature, y_feature, x_type, y_type)
