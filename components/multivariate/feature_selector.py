f2f005b3bc583c454ea1699b2e5f72d885cf30f8
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-14 12:07:04 +0530

    style(components/multivariate/feature-selector): change layout

--- components/multivariate/feature_selector.py
+++ components/multivariate/feature_selector.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from utils.feature_config import get_binary_columns, get_numeric_columns
+from utils.feature_config import get_feature_groups
 
 
 DEFAULT_FEATURES = [
@@ -21,25 +21,32 @@ def render(df):
         unsafe_allow_html=True,
     )
 
-    numerical_features = get_numeric_columns(df)
-    binary_features = get_binary_columns(df)
+    feature_groups = get_feature_groups(df)
+
+    numerical_features = feature_groups["Numerical"]
+    binary_features = feature_groups["Binary"]
+
     binary_features.remove("is_fraud")
 
     available_features = numerical_features + binary_features
 
-    selected_features = st.multiselect(
-        label="Select Features",
-        options=available_features,
-        default=DEFAULT_FEATURES,
-        max_selections=6,
-        placeholder="Choose up to 6 features...",
-    )
+    c1, c2 = st.columns([3, 0.5])
 
-    st.text_input(
-        label="Target Variable",
-        value="is_fraud",
-        disabled=True,
-    )
+    with c1:
+        selected_features = st.multiselect(
+            label="Select Features",
+            options=available_features,
+            default=DEFAULT_FEATURES,
+            max_selections=6,
+            placeholder="Choose up to 6 features...",
+        )
+
+    with c2:
+        st.text_input(
+            label="Target Variable",
+            value="is_fraud",
+            disabled=True,
+        )
 
     if len(selected_features) < 2:
         st.info(
