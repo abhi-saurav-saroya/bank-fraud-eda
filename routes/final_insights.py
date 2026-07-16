a885d46211462b84a330e0682eb23cc0b2d4db79
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-17 00:11:23 +0530

    feat(routes/final-insights): add a new tab

--- routes/final_insights.py
+++ routes/final_insights.py
@@ -5,6 +5,7 @@ from components.final_insights.correlation_with_fraud import render as render_co
 from components.final_insights.failed_attempts_insight import render as render_failed_attempts_insight
 from components.final_insights.hourly_fraud_insight import render as render_hourly_fraud_insight
 from components.final_insights.international_transactions_insight import render as render_international_transactions_insight
+from components.final_insights.merchant_category_insight import render as render_merchant_category_insight
 
 def render(df) -> None:
     """
@@ -21,6 +22,7 @@ def render(df) -> None:
             "🔐 Failed Attempts",
             "🕒 Hourly Fraud",
             "🌍 International",
+            "🏪 Merchant Category"
         ]
     )
 
@@ -29,6 +31,7 @@ def render(df) -> None:
         render_failed_attempts_insight,
         render_hourly_fraud_insight,
         render_international_transactions_insight,
+        render_merchant_category_insight
     ]
 
     for tab, renderer in zip(tabs, renderers):
