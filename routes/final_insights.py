efb84395b134ec33fc00b3ab806e76f7ec68a937
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-16 14:47:28 +0530

    style(routes/final-insights): change layout using tab structures

--- routes/final_insights.py
+++ routes/final_insights.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from components.final_insights.header import render as render_header
 from components.final_insights.correlation_with_fraud import render as render_correlation_with_fraud
+from components.final_insights.failed_attempts_insight import render as render_failed_attempts_insight
 
 def render(df) -> None:
     """
@@ -12,4 +13,10 @@ def render(df) -> None:
 
     st.divider()
 
-    render_correlation_with_fraud(df)
+    t1, t2, t3 = st.tabs(["Correlation with Fraud", "Fraud Rate by Failed Attempts", "Fraud Rate by Hour of Day"])
+
+    with t1:
+        render_correlation_with_fraud(df)
+
+    with t2:
+        render_failed_attempts_insight(df)
