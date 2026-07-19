624ccd80bbc415d9a37d6babd8c968e5da57c6aa
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-20 00:03:21 +0530

    feat(route/security-recommendations): render all cards

--- routes/security_recommendations.py
+++ routes/security_recommendations.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 from components.security_recommendations.header import render as render_header
 from components.security_recommendations.time_based_detection import render as render_time_based_detection
+from components.security_recommendations.failed_attempt_monitorring import render as render_failed_attempt_monitoring
+from components.security_recommendations.merchant_category_risk_scoring import render as render_merchant_category_risk_scoring
+from components.security_recommendations.pin_change_verification_policy import render as render_pin_change_verification_policy
 
 def render() -> None:
     """
@@ -13,3 +16,6 @@ def render() -> None:
     st.divider()
 
     render_time_based_detection()
+    render_failed_attempt_monitoring()
+    render_merchant_category_risk_scoring()
+    render_pin_change_verification_policy()
