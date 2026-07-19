61d05ac47a6bcce9c5922faf81a4d1043b825bcb
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-07-19 20:21:03 +0530

    fix(route/security-recommendations): remove extra parameter from render method

--- routes/security_recommendations.py
+++ routes/security_recommendations.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
 from components.security_recommendations.header import render as render_header
+from components.security_recommendations.time_based_detection import render as render_time_based_detection
 
-def render(df) -> None:
+def render() -> None:
     """
     Render the Security Recommendations page.
     """
 
     render_header()
+
+    st.divider()
+
+    render_time_based_detection()
